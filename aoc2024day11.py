febbec2a2e99a834fa0becaa2cba379e72df4ba7
sla-tur <sla.tur1999@gmail.com>
2024-12-12 20:10:00 +0000

    working on day 11

--- aoc2024day11.py
+++ aoc2024day11.py
@@ -5,11 +5,23 @@ def blink(elem, count, n, memo):
   count += 1
 
   if elem in memo.keys():
-    return blink(memo[elem], count+1, memo)
-  elif len(str(elem)) % 2 == 0:
-    mid = len(str(elem))//2
-    left = int(str(elem)[:mid])
-    right = int(str(elem)[mid:])
-    memo[elem] = blink(left, count, n, memo), blink(right, count, n, memo)
-    
+    for 
+      return memo[elem]
   
+  else:
+    if len(str(elem)) % 2 == 0:
+      mid = len(str(elem))//2
+      left = int(str(elem)[:mid])
+      right = int(str(elem)[mid:])
+      memo[elem] = blink(left, count, n, memo), blink(right, count, n, memo)
+    else:
+      memo[elem] = blink(elem*2024, count, n, memo)
+    
+file = open("aoc2024day11test.txt", "r")
+lst = [int(elem) for elem in file.readline().split()]
+print(lst)
+
+for i in range(len(lst)):
+  lst[i] = blink(lst[i], 0, 1, {0: 1})
+
+print(lst)
